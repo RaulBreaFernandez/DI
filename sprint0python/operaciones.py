ef55e4d3e5a19ff654b5b233dc79214aea69b875
RaulBreaFernandez <145338589+RaulBreaFernandez@users.noreply.github.com>
2023-09-26 20:44:23 +0200

    Add files via upload

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,22 @@
+def addition(num1, num2):
+
+    return num1+num2
+
+def subtraction(num1, num2):
+
+    return num1-num2
+
+def multiplication(num1, num2):
+
+    return num1 * num2
+
+def division(num1, num2):
+
+
+    if num2 == 0:
+        
+        print("No puedes divir algo entre cero")
+
+    else:
+
+        return num1/num2
