eafa94c7b49d6b4987d86ffd2227363f64c60ca0
RaulBreaFernandez <rbreaf@fpcoruna.afundacion.org>
2023-11-06 17:14:23 +0100

    Tarea 2. Botón navegar al detalle añadido

--- sprint3Android/MyCatalog/app/build.gradle.kts
+++ sprint3Android/MyCatalog/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.mycatalog"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mycatalog"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
